91d7c3b14eaf8a5f12724dfe82ae7a305a81b03c
ghost <ghost@localhost.localdomain>
2022-02-06 23:21:34 -0400

    reconocimiento texto y generacion txt para un conjunto de videos

--- scrap.py
+++ scrap.py
@@ -3,38 +3,41 @@ import time
 from PIL import Image
 import pytesseract
 from shutil import rmtree
+from pathlib import Path
 
 
-capture = cv2.VideoCapture('test.mp4') 
+path_videos = os.listdir("videos")
 
-fps = capture.get(cv2.CAP_PROP_FPS)
 
-cnt = 0
-time=0
+os.mkdir("logs")
 
-os.mkdir("aux")
-
-while (capture.isOpened()):
-    ret, frame = capture.read()
-    if (ret == True):
-        if(time>fps):
-            cv2.imwrite('aux/IMG_%04d.jpg' % cnt,frame)
-            cnt+=1
-            time=0
+for video in path_videos:
+    print(video)
+    capture = cv2.VideoCapture("videos/"+video) 
+    fps = capture.get(cv2.CAP_PROP_FPS)
+    cnt = 0
+    time=0
+    os.mkdir("aux")
+    while (capture.isOpened()):
+        ret, frame = capture.read()
+        if (ret == True):
+            if(time>fps):
+                cv2.imwrite('aux/IMG_%04d.jpg' % cnt,frame)
+                cnt+=1
+                time=0
+            else:
+              time+=1
         else:
-            time+=1
-    else:
-        break
-capture.release()
-cv2.destroyAllWindows()
+            break
+    capture.release()
+    cv2.destroyAllWindows()
+    
 
-caps = os.listdir("aux")
-for path_im in caps:
-    print(path_im)
-    img = Image.open("aux/"+path_im)
-    img.load()
-    text = pytesseract.image_to_string(img,config="-l spa --oem 1 --psm 1")
-    with open("log_v1.txt",'a') as log:
-        log.write(text)
-
-rmtree("aux")
+    caps = os.listdir("aux")
+    for path_im in caps:
+        img = Image.open("aux/"+path_im)
+        img.load()
+        text = pytesseract.image_to_string(img,config="-l spa --oem 1 --psm 1")
+        with open("logs/"+Path(video).stem+'.txt','a') as log:
+            log.write(text)
+    rmtree("aux")
